7ccc807c95d81afc3b7d55933040790c3ca9a5bb
luisinho07 <luisalberto.ramirezb@gmail.com>
2021-06-29 21:01:35 -0500

    Second class java code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
 public class App {
+
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private double estatura;
+    private float peso;
+    private boolean estadoCivil;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello world!");
+        int resultado = 15 + 32;
+        String res = "Hola";
+        int resultado2 = 125 - 32;
+        int resultado3 = 25 / 3;
+        double resultado4 = 58 * 21;
+        int resultado = 21 % 9;
+
+        //operadores unarios
+        resultado++;
+        resultado--;
+
+        // operadore lógicos
+        boolean respuesta5 = true;
+        respuesta5 = 5<8;
+        respuesta5 = 5>8;
+        respuesta5 = 5<=8;
+        respuesta5 = 5<=8;
+        respuesta5 = 5>=8;
+        respuesta5 = 5!=8;
+        respuesta5 = 5==8;
+        respuesta5 = 12 > 5 && 4 > 1
+
+        // estructuras de control
+        
     }
 }
